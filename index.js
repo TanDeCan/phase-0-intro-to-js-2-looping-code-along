44718ca9c3b794c945fcc40f18c93e6d882285fb
VictoriaBladow <vbladow9999@yahoo.com>
2022-11-13 16:14:39 -0600

    commit tree d0fd6448d65cfbdffa4ee6dd6ee63ee4b6c0f4fb

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
-// Code your solutions in this file
+function writeCards(wombo, event){
+    const names = []
+    for (let i = 0; i < wombo.length; i++) {
+        names.push(`Thank you, ${wombo[i]}, for the wonderful ${event} gift!`)
+    }
+    return names
+}
+function countDown(n){
+    let down = n;
+    while (down >= 0) {
+        console.log(down--);
+    }
+}
